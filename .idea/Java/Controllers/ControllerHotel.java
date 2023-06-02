08954d15fb0a00234968fb1d199ef45374a7476f
javif <javifdez195@gmail.com>
2023-06-02 20:18:25 +0200

    Primer cambio

---
+++ .idea/Java/Controllers/ControllerHotel.java
@@ -0,0 +1,64 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HotelController {
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField nombreField;
+
+    @FXML
+    private TextField direcciónField;
+
+    @FXML
+    private Button guardarButton;
+
+    // Instancia del DAO de Hotel
+    private HotelDAO hotelDAO;
+
+    // Método de inicialización del controlador
+    public void initialize() {
+        // Crear una instancia del DAO de Hotel
+        ConexiónBD conexiónBD = new ConexiónBDImpl();
+        hotelDAO = new HotelDAOImpl(conexiónBD);
+    }
+
+    // Método de acción para el botón "Guardar"
+    @FXML
+    private void guardarHotel() {
+        int id = Integer.parseInt(idField.getText());
+        String nombre = nombreField.getText();
+        String dirección = direcciónField.getText();
+
+        // Crear una instancia de Hotel
+        Hotel hotel = new Hotel(id, nombre, dirección);
+
+        // Guardar el hotel en la base de datos
+        hotelDAO.guardarHotel(hotel);
+
+        // Mostrar un mensaje de éxito
+        mostrarMensaje("Hotel guardado correctamente.");
+
+        // Limpiar los campos de entrada
+        limpiarCampos();
+    }
+
+    // Método para mostrar un mensaje de alerta
+    private void mostrarMensaje(String mensaje) {
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle("Información");
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+
+    // Método para limpiar los campos de entrada
+    private void limpiarCampos() {
+        idField.clear();
+        nombreField.clear();
+        direcciónField.clear();
+    }
+}
---
+++ .idea/Java/Model/Hotel.java
@@ -0,0 +1,58 @@
+import java.util.List;
+
+public class Hotel {
+    private int id; // Identificador único del hotel
+    private String nombre; // Nombre del hotel
+    private String dirección; // Dirección del hotel
+    private List<Habitación> habitaciones; // Lista de habitaciones del hotel
+
+    public Hotel(int id, String nombre, String dirección, List<Habitación> habitaciones) {
+        this.id = id;
+        this.nombre = nombre;
+        this.dirección = dirección;
+        this.habitaciones = habitaciones;
+    }
+
+    // Getter y Setter para el id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter y Setter para el nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter y Setter para la dirección
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+
+    // Getter y Setter para la lista de habitaciones
+    public List<Habitación> getHabitaciones() {
+        return habitaciones;
+    }
+
+    public void setHabitaciones(List<Habitación> habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
+    // Otros métodos de la clase Hotel
+
+    @Override
+    public String toString() {
+        return "Hotel [id=" + id + ", nombre=" + nombre + ", dirección=" + dirección + "]";
+    }
+}
